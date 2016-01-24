fc3e4f51b80b81c9b7ead50d395d981eeed5b5ed
Raúl Caro Pastorino <fryntiz@pandora>
2016-01-24 17:08:04 +0100

    Base HackTool

---
+++ 1-Hacker/0-wifi/dependencias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "No están aún definidas las dependencias"
+echo "Tales serán
+Suite aircrack
+pyrit
+cowpaty
+macchanger
+Librerias comunes como mdk"
+echo ""
+echo "Aunque esto solo son indicaciones o ejemplos"
