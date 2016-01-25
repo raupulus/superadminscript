cd3d5bfa2dc357272aa7f87890d95e6ca6708dbb
Raúl Caro Pastorino <fryntiz@pandora>
2016-01-25 11:49:44 +0100

    Base

--- Ordenar viejos/AutologinSSH.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#nmap -n -sS -O 192.168.1.0/24
-#ssh alumno@ip	"/etc/init.d/gdm3 stop"
-#apt-get install sshpass
-
-sshpass -p 1234 ssh root@192.168.1.42
-
-
-
--- Ordenar viejos/gdrive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Script para actualizar las carpetas de google drive con la cuenta raupulus@gmail.com
-
-##Carpeta "/home/nerviozzo/grive/1 - HTML"
-cd "/home/nerviozzo/grive"
-grive
-
-
-
