fb8fe5dddbb37cf09c2b834b0873d4c998528389
nerviozzo <raupulus@gmail.com>
2016-01-25 16:27:45 +0100

    Añadidas constantes para colores

--- actualizar.sh
+++ actualizar.sh
@@ -2,16 +2,27 @@
 
 #Variables Generales
 
-#Variables de colores
-MagentaNegrita='\033[35;1m';
-AzulNegrita='\033[34;1m';
-AmarilloNegrita='\033[33;1m';
-Verde='\033[32;22m';
-VerdeNegrita='\033[32;1m';
-Rojo='\033[31;22m';
-RojoNegrita='\033[31;1m';
-
-FinColor='\033[0;0m';
+##### CONSTANTES COLORES #####
+negro="\033[0;30m"
+rojo="\033[0;31m"
+verde="\033[0;32m"
+marron="\033[0;33m"
+azul="\033[0;34m"
+magenta="\033[0;35m"
+cyan="\033[01;36m"
+grisC="\033[0;37m"
+gris="\033[1;30m"
+rojoC="\033[1;31m"
+verdeC="\033[1;32m"
+amarillo="\033[1;33m"
+azulC="\033[1;34m"
+magentaC="\033[1;35m"
+cyanC="\033[1;36m"
+blanco="\033[1;37m"
+subrayar="\E[4m"
+parpadeoON="\E[5m"
+parpadeoOFF="\E[0m"
+resaltar="\E[7m"
 
 #Script para Actualizar
 #Comprobado funcionamiento solo en Debian rama Estable
