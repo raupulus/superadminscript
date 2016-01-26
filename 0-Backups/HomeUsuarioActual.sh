cc997c6c37ad4f41a91875263d1c1c0ed6b988c9
fryntiz <fryntiz@gmail.com>
2016-01-26 18:51:02 +0100

    Agregadas constantes y variables

--- 0-Backups/HomeUsuarioActual.sh
+++ 0-Backups/HomeUsuarioActual.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#Variables Generales
+UsuarioActual=$(whoami)
+DirInstalacion="Documentos/0-Scripts_2"
+LugarDeInstalacion="/home/$UsuarioActual/$DirInstalacion"
+
+##### CONSTANTES COLORES #####
+negro="\033[0;30m"
+rojo="\033[0;31m"
+verde="\033[0;32m"
+marron="\033[0;33m"
+azul="\033[0;34m"
+magenta="\033[0;35m"
+cyan="\033[01;36m"
+grisC="\033[0;37m"
+gris="\033[1;30m"
+rojoC="\033[1;31m"
+verdeC="\033[1;32m"
+amarillo="\033[1;33m"
+azulC="\033[1;34m"
+magentaC="\033[1;35m"
+cyanC="\033[1;36m"
+blanco="\033[1;37m"
+subrayar="\E[4m"
+parpadeoON="\E[5m"
+parpadeoOFF="\E[0m"
+resaltar="\E[7m"
+
 echo 'Este script aún no se ha definido';
 echo 'El objetivo de este script es pedir el nombre del usuario que se desea realizar la copia de seguridad como una variable de entrada';
 
