8781874896e456b187033a0ceb32a68ebd0ae99c
fryntiz <fryntiz@gmail.com>
2016-01-28 18:44:24 +0100

    Resaltado de colores para diferenciar situaciones del script y aclaradas las partes

--- 0-Backups/all_Backups.sh
+++ 0-Backups/all_Backups.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
-#Crear todas las copias de Backups
-echo "Comenzando Copia Home";
-sleep 2;
+#Variables Generales
+UsuarioActual=$(whoami)
+DirInstalacion="Documentos/0-Scripts_2"
+LugarDeInstalacion="/home/$UsuarioActual/$DirInstalacion"
+DirPreferencias="$LugarDeInstalacion/PREFERENCIAS/backups.pref"
+Preferencias="$(cat $DirPreferencias)"
+
+##### CONSTANTES COLORES #####
+negro="\033[0;30m"
+rojo="\033[0;31m"
+verde="\033[0;32m"
+marron="\033[0;33m"
+azul="\033[0;34m"
+magenta="\033[0;35m"
+cyan="\033[01;36m"
+grisC="\033[0;37m"
+gris="\033[1;30m"
+rojoC="\033[1;31m"
+verdeC="\033[1;32m"
+amarillo="\033[1;33m"
+azulC="\033[1;34m"
+magentaC="\033[1;35m"
+cyanC="\033[1;36m"
+blanco="\033[1;37m"
+subrayar="\E[4m"
+parpadeoON="\E[5m"
+parpadeoOFF="\E[0m"
+resaltar="\E[7m"
+
+#Crear Backup para directorio HOME con todos los datos de usuarios
+echo -e "$verdeC Comenzando Copia de Seguridad para$amarillo '/Home' $blanco"
+sleep 2
 #Backup para directorio Home
-sh BackupHome.sh;
+sh ./BackupHome.sh
 
 #Backup para todo el Sistema
-echo "Comenzando Copia Sistema";
-sleep 2;
-sh BackupSystem.sh;
+echo -e "$verdeC Comenzando Copia de Seguridad para$amarillo 'Sistema'$blanco"
+sleep 2
+sh ./BackupSystem.sh
 
 #Mensaje de finalización
-echo "Trabajo terminado";
-sleep 2;
+echo ""
+echo -e "$verdeC Trabajo terminado$blanco"
+echo ""
+echo -e "$verdeC Puedes comprobar si se ha realizado correctamente el BACKUP en el directorio elegido dentro de PREFERENCIAS:$rojoC $Preferencias $blanco"
+sleep 2
