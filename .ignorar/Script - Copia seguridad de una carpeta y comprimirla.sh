4a214da5ca78a1ce2b4195254014252424438f1c
fryntiz <fryntiz@gmail.com>
2016-01-29 13:17:13 +0100

    Eliminado directorio ".ignorar" que solo eran muestras de scripts anteriores

--- .ignorar/Script - Copia seguridad de una carpeta y comprimirla.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/usuario/Documentos/
-tar -czf "NOMBRE_UNA_VEZ_COMPRIMIDO"`date +%d%b%y`.tar "Carpetas_a_comprimir"
-mv "NOMBRE_UNA_VEZ_COMPRIMIDO".tar /home/usuario/Escritorio
-
