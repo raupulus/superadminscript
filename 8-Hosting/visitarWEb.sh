a37abdc423644ea64aa0e12c502fded4778eea7b
fryntiz <fryntiz@gmail.com>
2016-02-08 02:47:27 +0100

    Script para probar web con visitas terminado a falta de retocar estilos y depurar

--- 8-Hosting/visitarWEb.sh
+++ 8-Hosting/visitarWEb.sh
@@ -3,7 +3,9 @@
 #Variables
 repeticiones=""
 navegador=""
-maxPaginasSimultaneas="8"
+maxPaginasSimultaneas=""
+sitioweb=""
+contador="0"
 
 ##### CONSTANTES COLORES #####
 negro="\033[0;30m"
@@ -30,6 +32,63 @@ resaltar="\E[7m"
 clear
 
 #Pedir introducir las veces que se abrirá la página
-echo -e "$verdeC Selecciona el número$blanco"
+echo -e "$verdeC Selecciona el número de veces que se visitará la página$amarillo (Tiene que ser un número)$blanco"
 read repeticiones
-echo "$repeticiones"
+clear
+echo -e "$verdeC Escribe la página web o la dirección IP$amarillo (por ejemplo: www.laguialinux.es)$blanco"
+read sitioweb
+clear
+echo -e "$verdeC Cuantas páginas quieres abrir a la vez$amarillo (recomiendo 10 si no lo sabes)$blanco"
+read maxPaginasSimultaneas
+clear
+echo -e "$verdeC Selecciona un navegador para realizar las pruebas$amarillo (Por supuesto tiene que estar instalado)$blanco"
+echo -e "1 - Chrome"
+echo -e "2 - Chromium"
+echo -e "3 - Firefox"
+echo -e "4 - Opera"
+echo -e "5 - Iceweasel"
+echo -e "6 - Epiphany"
+read navegadorelegido
+	case $navegadorelegido in
+		1)
+			navegador="chrome";;
+		2)
+			navegador="chromium";;
+		3)
+			navegador="firefox";;
+		4)
+			navegador="opera";;
+		5)
+			navegador="iceweasel";;
+		6)
+			navegador="epiphany";;
+		*)
+			echo -e"$rojoC No existe esa opción$blanco"
+			exit 1;;
+	esac
+clear
+
+echo -e "$verdeC Se realizarán $rojoC$repeticiones$verdeC conexiones a la página web $rojoC$sitioweb$blanco"
+echo -e "$verdeC Pero a la vez solo se abriran $rojoC$maxPaginasSimultaneas$blanco"
+echo -e "$verdeC Para ello se utilizará el navegador $rojoC$navegador$blanco"
+echo ""
+echo -e "$amarillo Pulsa cualquier tecla si todo está correcto o$rojoC Control+C$amarillo para salir$blanco"
+read foo
+clear
+
+echo -e "$rojoC Realizando operaciones$blanco"
+while [ $contador -lt $( expr $repeticiones / $maxPaginasSimultaneas ) ]; do
+	echo "El contador es $contador"
+	echo ""
+		ContadorSimultaneo="0"
+		while [ $ContadorSimultaneo -lt $maxPaginasSimultaneas ]; do
+			$navegador $sitioweb&
+			let ContadorSimultaneo=ContadorSimultaneo+1
+		done
+	sleep 8
+	#contador=$( expr $contador + 1 )
+	let contador=contador+1
+	killall $navegador
+	sleep 1
+	clear
+done
