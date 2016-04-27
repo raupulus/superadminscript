c6df496c5c0003a5e6a627562f1a0edc4f51b2dc
fryntiz <fryntiz@gmail.com>
2016-04-27 21:02:19 +0200

    Renombrado error de nombre

--- 2-Rsync/Server_to_Ultrabook/3-LibreriasWeb.sh
+++ 2-Rsync/Server_to_Ultrabook/3-LibreriasWeb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Sincronizar carpeta Librerías Web
 
-rsync -av --delete --progress root@172.18.1.1:/media/nerviozzo/DATOS_2TB/3\\\ -\\\ Librerías\\\ WEB/ /home/fryntiz/1-MOUNT/DATOS/3\ -\ Mis\ Páginas\ webs/
+rsync -av --delete --progress root@172.18.1.1:/media/nerviozzo/DATOS_2TB/3\\\ -\\\ Librerías\\\ WEB/ /home/fryntiz/1-MOUNT/DATOS/3\ -\ Librerías\ WEB/
