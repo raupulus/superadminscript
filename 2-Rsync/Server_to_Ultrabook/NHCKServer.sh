bec6dc939772f6523caa731e0499ccc53ccc3189
fryntiz <fryntiz@gmail.com>
2016-04-28 01:41:55 +0200

    Estructura montada a falta de correcciones para una correcta conexión

--- 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
+++ 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-actual="1"
+actual="0";
+total="5";
 
 #Directorios en Remoto
 dirRemoto1="directorio remoto 1";
-dir2="";
-dir3="";
-dir4="";
+dirRemoto2="directorio remoto 2";
+dirRemoto3="directorio remoto 3";
+dirRemoto4="directorio remoto 4";
+dirRemoto5="directorio remoto 5";
+array1=( "$dirRemoto1" "$dirRemoto2" "$dirRemoto3" "$dirRemoto4" "$dirRemoto5" );
 
 #Directorios en local
 dirLocal1="directorio local 1";
-
+dirLocal2="directorio local 2";
+dirLocal3="directorio local 3";
+dirLocal4="directorio local 4";
+dirLocal5="directorio local 5";
+array2=( "$dirLocal1" "$dirLocal2" "$dirLocal3" "$dirLocal4" "$dirLocal5" );
 
 #Comando Rsync con opciones
 comando="rsync -av --delete --progress"
@@ -39,9 +46,12 @@ echo "Comando completo";
 echo "$comandoCompleto";
 
 
-while [ $actual -lt 10 ]; do
+while [ $actual -lt $total ]; do
 	echo "$actual";
-	let actual=actual+1
+	echo "${array1[$actual]} ${array2[$actual]}";
+
+	echo "$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocalCompleta${array2[$actual]}";
+	let actual=actual+1;
 done
 
 #"rsync -av --delete --progress root@172.18.1.1:/media/nerviozzo/DATOS_2TB/3\\\ -\\\ Librerías\\\ WEB/ /home/fryntiz/1-MOUNT/DATOS/3\ -\ Mis\ Páginas\ webs/"
