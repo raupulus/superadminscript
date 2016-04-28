df92804263a979ab44dbe2589c5716729d0f5fed
fryntiz <fryntiz@gmail.com>
2016-04-28 19:19:12 +0200

    Completado funcionando correcto

--- 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
+++ 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
@@ -3,7 +3,7 @@ actual="0";
 total="1";
 
 #Directorios en Remoto
-dirRemoto1="12\\\ -\\\ Otros/";
+dirRemoto1="12\ -\ Otros/";
 dirRemoto2="directorio remoto 2";
 dirRemoto3="directorio remoto 3";
 dirRemoto4="directorio remoto 4";
@@ -11,7 +11,7 @@ dirRemoto5="directorio remoto 5";
 array1=( "$dirRemoto1" "$dirRemoto2" "$dirRemoto3" "$dirRemoto4" "$dirRemoto5" );
 
 #Directorios en local
-dirLocal1="12\ -\ Otros/";
+dirLocal1="12 - Otros/";
 dirLocal2="directorio local 2";
 dirLocal3="directorio local 3";
 dirLocal4="directorio local 4";
@@ -29,29 +29,31 @@ servidor='172.18.1.1';
 rutaServidor="/home/$usuarioLocal/1-MOUNT/DATOS_2TB/";
 
 #Rutas completas
-rutaRemotaCompleta="$usuarioRemoto@$servidor$rutaServidor";
-rutaLocalCompleta="$usuarioLocal$rutaLocal";
+rutaRemotaCompleta="$usuarioRemoto@$servidor:$rutaServidor";
 
 #Comando completo con ruta Local y Remota
-comandoCompleto="$comando $rutaRemotaCompleta $rutaLocalCompleta";
+comandoCompleto="$comando $rutaRemotaCompleta $rutaLocal";
 
 #Comprobando rutas y comando
 echo "Ruta remota completa";
 echo "$rutaRemotaCompleta";
 echo "";
 echo "Ruta local Completa";
-echo "$rutaLocalCompleta";
+echo "$rutaLocal";
 echo "";
 echo "Comando completo";
 echo "$comandoCompleto";
+echo "";
+echo "Longitud del Array";
+echo ${#array1[@]}
 
 
 while [ $actual -lt $total ]; do
 	echo "$actual";
 	echo "${array1[$actual]} ${array2[$actual]}";
 
-	echo "$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocalCompleta${array2[$actual]}";
-	$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocalCompleta${array2[$actual]};
+	echo "$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocal${array2[$actual]}";
+	$comando "$rutaRemotaCompleta${array1[$actual]}" "$rutaLocal${array2[$actual]}";
 	let actual=actual+1;
 done
 
