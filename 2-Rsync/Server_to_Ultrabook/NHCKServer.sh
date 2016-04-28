460e2ceb9e35cdcdc3344f5fcd171c2863e09d8e
fryntiz <fryntiz@gmail.com>
2016-04-28 15:51:14 +0200

    Prueba con dir "Otros"

--- 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
+++ 2-Rsync/Server_to_Ultrabook/NHCKServer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 actual="0";
-total="5";
+total="1";
 
 #Directorios en Remoto
-dirRemoto1="directorio remoto 1";
+dirRemoto1="12\\\ -\\\ Otros/";
 dirRemoto2="directorio remoto 2";
 dirRemoto3="directorio remoto 3";
 dirRemoto4="directorio remoto 4";
@@ -11,7 +11,7 @@ dirRemoto5="directorio remoto 5";
 array1=( "$dirRemoto1" "$dirRemoto2" "$dirRemoto3" "$dirRemoto4" "$dirRemoto5" );
 
 #Directorios en local
-dirLocal1="directorio local 1";
+dirLocal1="12\ -\ Otros/";
 dirLocal2="directorio local 2";
 dirLocal3="directorio local 3";
 dirLocal4="directorio local 4";
@@ -30,7 +30,7 @@ rutaServidor="/home/$usuarioLocal/1-MOUNT/DATOS_2TB/";
 
 #Rutas completas
 rutaRemotaCompleta="$usuarioRemoto@$servidor$rutaServidor";
-rutaLocalCompleta="$usuarioLocal/$rutaLocal";
+rutaLocalCompleta="$usuarioLocal$rutaLocal";
 
 #Comando completo con ruta Local y Remota
 comandoCompleto="$comando $rutaRemotaCompleta $rutaLocalCompleta";
@@ -51,9 +51,12 @@ while [ $actual -lt $total ]; do
 	echo "${array1[$actual]} ${array2[$actual]}";
 
 	echo "$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocalCompleta${array2[$actual]}";
+	$comando $rutaRemotaCompleta${array1[$actual]} $rutaLocalCompleta${array2[$actual]};
 	let actual=actual+1;
 done
 
+
+#rsync -av --delete --progress root@172.18.1.1:/media/nerviozzo/DATOS_2TB/12\\\ -\\\ Otros/ /home/fryntiz/1-MOUNT/DATOS/12\ -\ Otros/
 #"rsync -av --delete --progress root@172.18.1.1:/media/nerviozzo/DATOS_2TB/3\\\ -\\\ Librerías\\\ WEB/ /home/fryntiz/1-MOUNT/DATOS/3\ -\ Mis\ Páginas\ webs/"
 
 
