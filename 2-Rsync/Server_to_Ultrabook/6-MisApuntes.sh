a2b3704836dcb120ee495b47834cc2df4243c763
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:16:07 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/6-MisApuntes.sh
+++ 2-Rsync/Server_to_Ultrabook/6-MisApuntes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Mis Apuntes
+#Sincronizar directorio Mis Apuntes
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/6-Mis_Apuntes/ /home/fryntiz/0-MOUNT/DATOS/6-Mis_Apuntes\
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/6-Mis_Apuntes/ /home/fryntiz/0-MOUNT/DATOS/6-Mis_Apuntes\
