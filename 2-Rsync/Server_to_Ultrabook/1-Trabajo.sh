2fdb977f44b8b3869117e698d6c533bb48876809
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:15:22 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/1-Trabajo.sh
+++ 2-Rsync/Server_to_Ultrabook/1-Trabajo.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Sincronizar carpeta Proyectos WEB
-
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/1-Trabajo/ /home/fryntiz/0-MOUNT/DATOS/1-Trabajo/
+#Sincronizar Directorio de Trabajo
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/1-Trabajo/ /home/fryntiz/0-MOUNT/DATOS/1-Trabajo/
