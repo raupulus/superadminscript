ddda56ca0f9924711af88af8507411dcd8dddd9f
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:16:11 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/7-ArchivosPersonales.sh
+++ 2-Rsync/Server_to_Ultrabook/7-ArchivosPersonales.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Archivos Personales
+#Sincronizar directorio Archivos Personales
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/7-Archivos_Personales/ /home/fryntiz/0-MOUNT/DATOS/7-Archivos_Personales/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/7-Archivos_Personales/ /home/fryntiz/0-MOUNT/DATOS/7-Archivos_Personales/
