083709954415e97af1d8ae23a14dd6f888602ef7
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:16:35 +0100

    Limpiado comentarios antiguos

--- 2-Rsync/Server_to_Ultrabook/home-proyectos-video.sh
+++ 2-Rsync/Server_to_Ultrabook/home-proyectos-video.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-#Sincronizar carpeta proyectos de vídeos
-
-#rsync -av --delete root@192.168.0.254:/home/nerviozzo/Documentos /media/DATOS_/PRUEBA_RSYNC/
-
-
-#rsync -av --delete --progress -e 'ssh -p 22222' root@172.18.8.120:/home/alumno/Descargas /home/nerviozzo/Descargas/rsync
-
-#rsync -av --delete --progress root@192.168.0.254:/home/nerviozzo/Proyectos\\\ de\\\ Vídeos/ /media/DATOS/Home\ -\ Proyectos\ de\ Vídeos/
+#Sincronizar directorio Proyectos de Vídeos
