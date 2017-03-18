ed160dc9d1122b5650690c5ced8b8adc99a678db
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:15:53 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/3-MisPaginasWebs.sh
+++ 2-Rsync/Server_to_Ultrabook/3-MisPaginasWebs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Mis Páginas webs
+#Sincronizar directorio Mis Páginas webs
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/3-Mis_Páginas_WEBS/ /home/fryntiz/0-MOUNT/DATOS/3-Mis_Páginas_WEBS/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/3-Mis_Páginas_WEBS/ /home/fryntiz/0-MOUNT/DATOS/3-Mis_Páginas_WEBS/
