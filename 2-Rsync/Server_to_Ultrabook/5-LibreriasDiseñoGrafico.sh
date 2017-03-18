8bb0de3df1e76273fc98aaea01e410258e0564f0
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:16:03 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/5-LibreriasDiseñoGrafico.sh
+++ 2-Rsync/Server_to_Ultrabook/5-LibreriasDiseñoGrafico.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Librerías de diseño Gráfico
-#rsync -av --delete root@192.168.0.254:/home/nerviozzo/Documentos /media/DATOS_/PRUEBA_RSYNC/
+#Sincronizar directorio Librerías de diseño Gráfico
 
-
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/5-Librerías_Diseño_Gráfico/ /home/fryntiz/0-MOUNT/DATOS/5-Librerías_Diseño_Gráfico/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/5-Librerías_Diseño_Gráfico/ /home/fryntiz/0-MOUNT/DATOS/5-Librerías_Diseño_Gráfico/
