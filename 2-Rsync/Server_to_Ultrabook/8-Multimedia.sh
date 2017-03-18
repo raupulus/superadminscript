1f9acb2fd480e4d24c66ee46f30d5e29a2549a97
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:16:15 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/8-Multimedia.sh
+++ 2-Rsync/Server_to_Ultrabook/8-Multimedia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Multimedia
+#Sincronizar directorio Multimedia
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/8-Multimedia/ /home/fryntiz/0-MOUNT/DATOS/8-Multimedia/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/8-Multimedia/ /home/fryntiz/0-MOUNT/DATOS/8-Multimedia/
