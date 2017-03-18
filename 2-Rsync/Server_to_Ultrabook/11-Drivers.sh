5f2369cee3f3597b6f63823da6ee162e55676eee
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:15:32 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/11-Drivers.sh
+++ 2-Rsync/Server_to_Ultrabook/11-Drivers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Programas
+#Sincronizar directorio Drivers
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/11-Drivers/ /home/fryntiz/0-MOUNT/DATOS/11-Drivers/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/11-Drivers/ /home/fryntiz/0-MOUNT/DATOS/11-Drivers/
