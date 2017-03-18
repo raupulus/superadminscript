aa1e2b40098d592ff541a171d77ad41e4f3b11b1
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:15:58 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/4-LibreriasWeb.sh
+++ 2-Rsync/Server_to_Ultrabook/4-LibreriasWeb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Librerías Web
+#Sincronizar directorio Librerías Web
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/4-Librerías_WEB/ /home/fryntiz/0-MOUNT/DATOS/4-Librerías_WEB/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/4-Librerías_WEB/ /home/fryntiz/0-MOUNT/DATOS/4-Librerías_WEB/
