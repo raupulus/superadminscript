1f36b569ea790226abea05b81a83ee07cc84578a
fryntiz <tecnico@fryntiz.es>
2017-03-18 18:15:27 +0100

    Sincronización mejorada, añadida compresión y exclusión

--- 2-Rsync/Server_to_Ultrabook/10-Programas.sh
+++ 2-Rsync/Server_to_Ultrabook/10-Programas.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar carpeta Programas
+#Sincronizar directorio Programas
 
-rsync -av --delete --progress root@172.18.1.1:/media/fryntiz/DATOS_2TB/10-Programas/ /home/fryntiz/0-MOUNT/DATOS/10-Programas/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/10-Programas/ /home/fryntiz/0-MOUNT/DATOS/10-Programas/
