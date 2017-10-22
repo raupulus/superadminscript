3dd230c294db65b1f144ff978d18d59cd50248cd
fryntiz <tecnico@fryntiz.es>
2017-10-23 00:38:47 +0200

    Ampliadas opciones

--- 1_Actualizar_Software/main.sh
+++ 1_Actualizar_Software/main.sh
@@ -21,16 +21,39 @@ while :
         echo ""
         echo -e "             $amarillo $MenuActual $rojo   $version"
         echo ""
-        echo -e "   $rojo 1)  $verde op1"
+        echo -e "   $rojo 1)  $verde Aplicar todas las actualizaciones siguientes"
+        echo -e "   $rojo 1)  $verde Actualizar Repositorios del Sistema"
+        echo -e "   $rojo 2)  $verde Actualizar paquetes FlatPak"
+        echo -e "   $rojo 3)  $verde Actualizar paquetes Snap"
         echo -e "   $rojo 0)  $verde Volver atrás$gris"
         echo ""
 
     read -p "  → " OPCION
     case $OPCION in
 
-        1)  # Op1
+        1)  # Todas las actualizaciones
             clear
-            . $DirInstalacion/1_Actualizar_Software/????
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_Repositorios.sh
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_FLATPAK.sh
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_SNAP.sh
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        2)  # Actualizar Repositorios del Sistema
+            clear
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_Repositorios.sh
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        3)  # Actualizar paquetes FlatPak
+            clear
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_FLATPAK.sh
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        4)  # Actualizar paquetes Snap
+            clear
+            . $DirInstalacion/1_Actualizar_Software/Actualizar_SNAP.sh
             read -p "Pulsa una tecla para continuar" foo
             continue;;
 
