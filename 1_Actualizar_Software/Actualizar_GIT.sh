b6a3875f2b7d5a138fe326680ecfa43b51bf4c96
fryntiz <tecnico@fryntiz.es>
2017-10-23 00:45:24 +0200

    Comentarios de como continuar el script

--- 1_Actualizar_Software/Actualizar_GIT.sh
+++ 1_Actualizar_Software/Actualizar_GIT.sh
@@ -8,18 +8,8 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
-############################
-##   Constantes Colores   ##
-############################
-amarillo="\033[1;33m"
-azul="\033[1;34m"
-blanco="\033[1;37m"
-cyan="\033[1;36m"
-gris="\033[0;37m"
-magenta="\033[1;35m"
-rojo="\033[1;31m"
-verde="\033[1;32m"
-
 #############################
 ##   Variables Generales   ##
 #############################
+
+# Crear función que lea de una lista todos los repositorios del sistema
