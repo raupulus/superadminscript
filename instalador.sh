86770f133822689c6bac9c6bba234610dec2f1a2
fryntiz <tecnico@fryntiz.es>
2017-10-23 10:26:50 +0200

    fix tabs/whitespaces

--- instalador.sh
+++ instalador.sh
@@ -27,28 +27,6 @@ UsuarioActual=$(whoami)
 DirInstalacion="Documentos/0-Scripts_2"
 LugarDeInstalacion="/home/$UsuarioActual/$DirInstalacion"
 
-##### CONSTANTES COLORES #####
-negro="\033[0;30m"
-rojo="\033[0;31m"
-verde="\033[0;32m"
-marron="\033[0;33m"
-azul="\033[0;34m"
-magenta="\033[0;35m"
-cyan="\033[01;36m"
-grisC="\033[0;37m"
-gris="\033[1;30m"
-rojoC="\033[1;31m"
-verdeC="\033[1;32m"
-amarillo="\033[1;33m"
-azulC="\033[1;34m"
-magentaC="\033[1;35m"
-cyanC="\033[1;36m"
-blanco="\033[1;37m"
-subrayar="\E[4m"
-parpadeoON="\E[5m"
-parpadeoOFF="\E[0m"
-resaltar="\E[7m"
-
 #Script para Instalar en el Sistema Operativo
 #Comprobado funcionamiento solo en Debian rama Estable
 #Único idioma disponible de momento ESPAÑOL de ESPAÑA
@@ -62,15 +40,15 @@ echo -e "Pulsa$subrayar$verdeC 'Y' $grisC para continuar"
 #Aquí introducir condicional (if echo $SHELL == /bin/bash) {exit o continue}else ------ rm /bin/sh | ln -s /bin/bash /bin/sh o así:
 # sudo usermod -s /bin/bash $UsuarioActual
 if [ $SHELL == "/bin/bash" ]
-	then
-		echo -e "El SHELL es /bin/bash"
-	else
-		echo -e "No se encuentra instalado BASH, procediendo a su actualización"
-		sudo apt-get update
-		sudo apt-get install bash
-		sudo rm /bin/sh
-		sudo ln -s /bin/bash /bin/sh
-		sudo usermod -s /bin/bash $UsuarioActual
+    then
+        echo -e "El SHELL es /bin/bash"
+    else
+        echo -e "No se encuentra instalado BASH, procediendo a su actualización"
+        sudo apt-get update
+        sudo apt-get install bash
+        sudo rm /bin/sh
+        sudo ln -s /bin/bash /bin/sh
+        sudo usermod -s /bin/bash $UsuarioActual
 fi
 
 #Mostrar el usuario que soy y la ruta donde se instalará el menú
@@ -90,20 +68,20 @@ sleep 1
 #Remover directorio de instalación si ya existe
 echo -e "Limpiando instalación anterior si la hubiera"
 if [ -d $LugarDeInstalacion ]
-	then
-		echo -e "Existe el lugar de instalación, procediendo a borrarlo"
-		sleep 1
-		sudo rm -R -f $LugarDeInstalacion
-		mkdir $LugarDeInstalacion
-		sudo echo -e "$LugarDeInstalacion ha sido limpiado"
-		sleep 1
-	else
-		echo -e "El destino de la instalación no existe, se procede a crearlo"
-		sleep 1
-		mkdir $LugarDeInstalacion
-		echo -e "$LugarDeInstalacion ha sido creado"
-		whoami
-		pwd
+    then
+        echo -e "Existe el lugar de instalación, procediendo a borrarlo"
+        sleep 1
+        sudo rm -R -f $LugarDeInstalacion
+        mkdir $LugarDeInstalacion
+        sudo echo -e "$LugarDeInstalacion ha sido limpiado"
+        sleep 1
+    else
+        echo -e "El destino de la instalación no existe, se procede a crearlo"
+        sleep 1
+        mkdir $LugarDeInstalacion
+        echo -e "$LugarDeInstalacion ha sido creado"
+        whoami
+        pwd
 fi
 
 #Copiar programa dentro del directorio creado
