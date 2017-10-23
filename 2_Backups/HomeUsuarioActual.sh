83217cdadbec65759251f70b0fbc65bd811d8571
fryntiz <tecnico@fryntiz.es>
2017-10-23 13:01:25 +0200

    Cifrar backup creado

--- 2_Backups/HomeUsuarioActual.sh
+++ 2_Backups/HomeUsuarioActual.sh
@@ -48,15 +48,12 @@ function crearBackup() {
     sudo 7z a -t7z -r -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mhe=on -p$password /home/fryntiz/8_Backups/0-TMP/$nombreBackup.7z -x{!0-MOUNT,!1_GIT,!1-MOUNT,!2_Bases_de_Datos,!3_Librerías,!4_Programas,!5_Entornos_de_Trabajo,!6_Máquinas_Virtuales,!7_Mis_Proyectos,!8_Backups,!9_Dropbox,!10_GoogleDrive,!11_CloudStation,!12_Pentesting,!Descargas,!Documentos,!Imágenes,!NHCK,!Plantillas,!Vídeos,!.PlayOnLinux/wineprefix,!.local/share/Trash} -xr{!lost+found,!.cache,!.trash} /home/fryntiz/
 }
 
-sudo chown $UsuarioActual:$UsuarioActual $nombreBackup.7z
-mv ~/8_Backups/0-TMP/$nombreBackup.7z ~/8_Backups/2_PC_Sobremesa/Raíz/$nombreBackup.7z
+function cifrarBackup() {
+    sudo chown $UsuarioActual:$UsuarioActual $nombreBackup.7z
+    mv ~/8_Backups/0-TMP/$nombreBackup.7z ~/8_Backups/2_PC_Sobremesa/Raíz/$nombreBackup.7z
 
-whoami
-echo $UsuarioActual
+    whoami
+    echo $UsuarioActual
+}
 #Finalizando
-echo -e "$magentaC Se ha completado la copia de seguridad$grisC"
-echo -e "$grisC"
-
-
-#tar -cvpjf "$Preferencias/backup_HOME_AÑO`date +%y`_MES`date +%b`_DIA`date +%d`.tar.bz2" --exclude=lost+found --exclude=backup*.tar.bz2 --exclude=.trash --exclude=1-MOUNT --exclude=.cache --exclude=.trash --exclude=Documentos --exclude=Imágenes /home
-#tar -cvpjf "$Preferencias/backup_"$UsuarioActual"_AÑO`date +%y`_MES`date +%b`_DIA`date +%d`.tar.bz2" --exclude=lost+found --exclude=backup*.tar.bz2 --exclude=.trash --exclude=1-MOUNT --exclude=.cache --exclude=.trash --exclude=Documentos --exclude=Imágenes /home/$UsuarioActual
+echo -e "$magenta Se ha completado la copia de seguridad$gris"
