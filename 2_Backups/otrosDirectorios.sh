dbc50d88b3848b10d10c722eeb89e423a47913de
fryntiz <tecnico@fryntiz.es>
2017-10-23 13:03:58 +0200

    Creada estructura para el script

--- 2_Backups/otrosDirectorios.sh
+++ 2_Backups/otrosDirectorios.sh
@@ -8,6 +8,49 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+
 #############################
 ##   Variables Generales   ##
 #############################
+password=""
+excluirDeBackup="-x{!home,!tmp,!var/log/,!proc,!mnt,!sys/,!media/,!run/media/,!dev/pts,!run/user/1000/} -xr{!lost+found,!.cache,!.trash}"
+
+#Directorio: /home/backups /backups???
+Preferencias="$(cat $DirPreferencias/backups.pref)"
+
+NombreBackup="$Preferencias/0-TMP/backup_HOME_AÑO`date +%y`_MES`date +%b`_DIA`date +%d`.tar.bz2"
+NombreBackupCifrado="$Preferencias/backup_HOME_AÑO`date +%y`_MES`date +%b`_DIA`date +%d`.7z"
+
+clear
+
+function crearPassword() {
+    read -p "Introduce la contraseña de cifrado → " password1
+
+    clear
+
+    read -p "Introduce la contraseña de cifrado → " password2
+
+    if [ $password1 = $password2 ]
+    then
+        echo -e "$amarillo Las dos claves coinciden$gris"
+        echo -e "$azul Comenzará el Backup$gris"
+        password=$password1
+        sleep 2
+    else
+        echo -e "$amarillo No coinciden las dos contraseñas"
+        echo -e " Abortando Backup $gris"
+        exit 1
+    fi
+}
+crearPassword
+
+function crearBackup() {
+
+}
+
+function cifrarBackup() {
+
+}
+
+#Finalizando
+echo -e "$magenta Se ha completado la copia de seguridad$gris"
