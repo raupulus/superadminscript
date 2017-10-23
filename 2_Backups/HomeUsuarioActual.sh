2ee2e4717f6e2f5f161b5704b125ce76f3fe6a00
fryntiz <tecnico@fryntiz.es>
2017-10-23 13:00:13 +0200

    Añadida función para crear backup

--- 2_Backups/HomeUsuarioActual.sh
+++ 2_Backups/HomeUsuarioActual.sh
@@ -44,8 +44,9 @@ function crearPassword() {
 }
 crearPassword
 
-
-sudo 7z a -t7z -r -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mhe=on -p$password /home/fryntiz/8_Backups/0-TMP/$nombreBackup.7z -x{!0-MOUNT,!1_GIT,!1-MOUNT,!2_Bases_de_Datos,!3_Librerías,!4_Programas,!5_Entornos_de_Trabajo,!6_Máquinas_Virtuales,!7_Mis_Proyectos,!8_Backups,!9_Dropbox,!10_GoogleDrive,!11_CloudStation,!12_Pentesting,!Descargas,!Documentos,!Imágenes,!NHCK,!Plantillas,!Vídeos,!.PlayOnLinux/wineprefix,!.local/share/Trash} -xr{!lost+found,!.cache,!.trash} /home/fryntiz/
+function crearBackup() {
+    sudo 7z a -t7z -r -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mhe=on -p$password /home/fryntiz/8_Backups/0-TMP/$nombreBackup.7z -x{!0-MOUNT,!1_GIT,!1-MOUNT,!2_Bases_de_Datos,!3_Librerías,!4_Programas,!5_Entornos_de_Trabajo,!6_Máquinas_Virtuales,!7_Mis_Proyectos,!8_Backups,!9_Dropbox,!10_GoogleDrive,!11_CloudStation,!12_Pentesting,!Descargas,!Documentos,!Imágenes,!NHCK,!Plantillas,!Vídeos,!.PlayOnLinux/wineprefix,!.local/share/Trash} -xr{!lost+found,!.cache,!.trash} /home/fryntiz/
+}
 
 sudo chown $UsuarioActual:$UsuarioActual $nombreBackup.7z
 mv ~/8_Backups/0-TMP/$nombreBackup.7z ~/8_Backups/2_PC_Sobremesa/Raíz/$nombreBackup.7z
