2820d7eef04191945d1bbf3aadd4389127272e62
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-13 01:06:02 +0200

    Correcciones

--- 7-Rsync/Server_to_Ultrabook/3-LibreriasWeb.sh
+++ 7-Rsync/Server_to_Ultrabook/3-LibreriasWeb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Sincronizar directorio Librerías Web
 
-rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/4-Librerías_WEB/ /home/fryntiz/0-MOUNT/DATOS/3-Librerías_WEB/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/3-Librerías_WEB/ /home/fryntiz/0-MOUNT/DATOS/3-Librerías_WEB/
--- 7-Rsync/Server_to_Ultrabook/6-ManualesProductos.sh
+++ 7-Rsync/Server_to_Ultrabook/6-ManualesProductos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Sincronizar directorio ISOS
+#Sincronizar directorio Manuales
 
-rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/6-ManualesProductos/ /home/fryntiz/0-MOUNT/DATOS/6-ManualesProductos/
+rsync -avzt --delete --progress --exclude '.SynologyWorkingDirectory' root@172.18.1.1:/media/fryntiz/DATOS_2TB/6-Manuales_Productos/ /home/fryntiz/0-MOUNT/DATOS/6-Manuales_Productos/
